6a19e832a5d33d1f5297c0f4445cd04f84040a2a
imagalhaes <imagalhaes@redlabel>
2017-11-15 20:17:30 -0200

    Initial commit of the challenge.

---
+++ nasa-robot/src/main/java/com/github/igmfilho/challenge/nasarobot/App.java
@@ -0,0 +1,13 @@
+package com.github.igmfilho.challenge.nasarobot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
